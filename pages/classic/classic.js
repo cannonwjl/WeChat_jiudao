b5e7524d3a95ed2eb0d2f960b6d55ebcac428738
cannonwjl <1428943841@qq.com>
2018-08-14 23:11:15 +0800

    第三次 2018-8-14 第7章7节箭头函数内容

--- pages/classic/classic.js
+++ pages/classic/classic.js
@@ -5,17 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    test:1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(this.data.test);
        wx.request({
          url: 'http://bl.7yue.pro/v1/classic/latest',
          header:{
            appkey:"pe6lyAeHjpf7FrQf"
+         },
+         //箭头函数
+         success:(res)=>{
+           console.log(this.data.test)
          }
        })
   },
