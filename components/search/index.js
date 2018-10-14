fd8340404f994a41fe3413115738b455b9ee2304
cannonwjl <1428943841@qq.com>
2018-10-14 16:37:17 +0800

    2018-10-14 finished 15chapter

--- components/search/index.js
+++ components/search/index.js
@@ -1,16 +1,27 @@
 // components/search/index.js
-import {KeywordModel} from '../../models/keyword.js'
-import { BookModel } from '../../models/book.js'
-const keywordModel=new KeywordModel()
+import {
+  KeywordModel
+} from '../../models/keyword.js'
+import {
+  BookModel
+} from '../../models/book.js'
+import {
+  paginationBev
+} from "../behaviors/pagination.js"
+// "../behaviours/pagination.js"
+const keywordModel = new KeywordModel()
 const bookModel = new BookModel()
 Component({
   /**
    * 组件的属性列表
    */
+  behaviors: [paginationBev],
   properties: {
-    more:{ 
-      type:String,
-      observer:'_load_more'
+
+    more: {
+      type: String,
+
+      observer: 'loadmore'
     }
   },
 
@@ -18,16 +29,18 @@ Component({
    * 组件的初始数据
    */
   data: {
-     historyWords:[],
-     hotWords:[],
-     dataArray:[],
-     searching:false,
-     q:""
+    historyWords: [],
+    hotWords: [],
+    // dataArray:[],
+    searching: false,
+    q: "",
+    loading: false,
+    loadingCenter: false
   },
 
   // 生命周期函数 在组件初始化是调用
   attached() {
-                      // KeywordModel
+    // KeywordModel
     //const historyWords=keywordModel.getHistory()
     //const hotWords=keywordModel.getHot()
     //console.log(historyWords)
@@ -38,47 +51,129 @@ Component({
 
     keywordModel.getHot().then(res => {
       this.setData({
-        hotWords:res.data.hot
+        hotWords: res.data.hot
       })
-     // console.log("+++++++++++++++++++++++++++++++++++++" + hotWords)
+      // console.log("+++++++++++++++++++++++++++++++++++++" + hotWords)
     })
   },
   /**
    * 组件的方法列表
    */
   methods: {
-    _load_more(event)
-    {
-        console.log(1232)
-    },
-    onCancel(event)
-    {
-      this.triggerEvent('cancel',{},{})
-    },
-    onDelete(event)
-    {
+    loadmore(event) {
+     // console.log(1232)
+      if (!this.data.q) {
+        return
+      }
+      if (this.isLocked()) {
+        return
+      }
+      // 同时发送俩个请去 一次只请求
+      // const length = this.data.dataArray.length
 
-      this.setData({
-        searching: false
-      })
+      if (this.hasMore()) {
+        this.Locked()
+        bookModel.search(this.getCurrentStart(), this.data.q)
+          .then(res => {
+            //const tempArray=this.data.dataArray.concat(res.data.books)
+            this.setMoreData(res.data.books)
+            // this.setData({
+            //   dataArray: tempArray,
+            // })
+            this.unLocked()
+            // 
+
+            // this.unLocked()
+          }, () => {
+            this.unLocked()
+          })
+      }
+
+      // 死锁
+      //this.data.dataArray
+      //res.data.book
+      // this.setMoreData(res.data.books)
+
+      // const tempArray=this.data.dataArray.concat(res.data.books)
+      // this.setData({
+      //   dataArray:tempArray,
+      //   loading:false
+      // })
+      // })
+      //20
+      //20
+      //40   60 
+
+    },
+    onCancel(event) {
+      this.triggerEvent('cancel', {}, {})
+      this.initialize()
+    },
+    onDelete(event) {
+      this.initialize()
+      this._closeResult()
+
     },
 
-    onConfirm(event)
-    {
-     // const word=event.detail.value
+    onConfirm(event) {
+      // const word=event.detail.value
+      // this.setData({
+      //   searching: true
+      // })
+      this._showResult()
+      this.initialize()
+      this._showLoadingCenter()
+      const q = event.detail.value || event.detail.text
       this.setData({
-        searching:true
+        q: q
       })
-      const q=event.detail.value || event.detail.text
-      bookModel.search(0,q)
-      .then(res=>{
-        this.setData({
-          dataArray: res.data.books,
-          q:q
+      bookModel.search(0, q)
+        .then(res => {
+          this.setMoreData(res.data.books)
+          this.setTotal(res.data.total)
+         
+          keywordModel.addToHistory(q)
+          this._hideLoadingCenter()
         })
-          
+
+    },
+    _showResult()
+    {
+      this.setData({
+        searching: true
       })
-      keywordModel.addToHistory(q)
+    },
+    _closeResult() {
+      this.setData({
+        searching: false,
+        q:''
+      })
+    },
+    _isLocked(){
+      return this.data.loading?true:false
+    },
+    //这里写了locked但是调用了behavior下的Locked
+    _locked(){
+      //this.data.loading=true
+      this.setData({
+        loading: false
+      })
+          },
+     _unlocked() {
+      //this.data.loading = false
+      this.setData({
+        loading:false
+      })
+    },
+    _showLoadingCenter(){
+      this.setData({
+        loadingCenter:true
+      })
+    },
+    _hideLoadingCenter() {
+      this.setData({
+        loadingCenter: false
+      })
     }
   }
 })
